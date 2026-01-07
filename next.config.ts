60b68c849c59f1a02ceefe2c9b761675e8c3c832
olganedelcu <olganedelcuam@gmail.com>
2026-01-07 18:20:42 +0200

    fixed paths for assets

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
   reactCompiler: true,
+  basePath: isProd ? '/Marketing_Manager_Portfolio' : '',
+  assetPrefix: isProd ? '/Marketing_Manager_Portfolio' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
