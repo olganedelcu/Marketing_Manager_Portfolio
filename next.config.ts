2eb5a46c204328fb1354d910a5934a5fd9759e67
olganedelcu <olganedelcuam@gmail.com>
2026-01-10 23:12:26 +0200

    clean up

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: 'export',
   reactCompiler: true,
-  basePath: isProd ? '/Marketing_Manager_Portfolio' : '',
-  assetPrefix: isProd ? '/Marketing_Manager_Portfolio' : '',
   images: {
     unoptimized: true,
   },
